360ecc3e8aa38f01f5d9d716836c3fb14efb2b3e
Alan Wheatley <mr.rubikscube@mail.ru>
2023-02-09 16:18:02 +0500

    Add Js file

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin_all_from "app/javascript/custom", under: "custom"
+
+pin "jquery", to: "jquery3.min.js", preload: true
