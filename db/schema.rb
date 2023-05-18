9d3abc7df288ccae49d5a9bf97581b8b6384a3a3
Alan Wheatley <mr.rubikscube@mail.ru>
2023-05-18 08:09:08 +0500

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -49,11 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_122915) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "number"
